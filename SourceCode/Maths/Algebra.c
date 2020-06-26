159a7242dd366d1ca432393ac077b61632d2ba99
Purshotam1 <singhpurshotam48@gmail.com>
2020-06-26 11:01:56 +0530

    Added New Features 	Added mul Function 	Added quadratic and linear Equation Solvers

---
+++ SourceCode/Maths/Algebra.c
@@ -0,0 +1,7 @@
+void quadraticEquation(float a, float b, float c) {
+	//Calculate Roots of Quaratic Equation
+}
+
+void linearEquation(float a1, float b1, float c1, float d1, float e1) {
+	//Solve simultaneous linear equation
+}
